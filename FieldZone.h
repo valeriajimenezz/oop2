36b01aecd3b2870befb4b9074786a11c3f732dd7
valeriajimenezz <a01714375@tec.mx>
2026-06-14 12:38:01 -0600

    Update FieldZone.h

--- FieldZone.h
+++ FieldZone.h
@@ -1,4 +1,3 @@
-//FieldZone 
 #ifndef FIELDZONE_H
 #define FIELDZONE_H
 #include<iostream>
@@ -6,28 +5,40 @@
 #include<stdexcept>
 
 class FieldZone{
-	private:
-		std::string id;
-		double moistureLevel;
-		double nutrientLevel;
-	public:
-		//Constructor
-	FieldZone(std::string id, double moistureLevel, double nutrientLevel): id(id), moistureLevel(moistureLevel), nutrientLevel(nutrientLevel){
-		if (moistureLevel < 0 || moistureLevel > 100)
-    	throw std::invalid_argument("moistureLevel must be between 0 and 100");		
-	}
-	//Getters 
-	double getmoistureLevel() const {return moistureLevel;}
-	double getnutrientLevel() const {return nutrientLevel;}
-	std::string getId() const {return id;}
-	
-	// Methods 
-	
-	void display() const {
-		std::cout<<"Zone: "<<id << "\n";
-		std::cout<<"Moisture Level: " <<moistureLevel<< "/100 \n";
-		std::cout<<"Nutrient Level: " <<nutrientLevel<< "/100 \n";
-	
-	}
+    private:
+        std::string id;
+        double moistureLevel;
+        double nutrientLevel;
+    public:
+        //Constructor
+        FieldZone(std::string id, double moistureLevel, double nutrientLevel)
+            : id(id), moistureLevel(moistureLevel), nutrientLevel(nutrientLevel){
+            if (moistureLevel < 0 || moistureLevel > 100)
+                throw std::invalid_argument("moistureLevel must be between 0 and 100");
+            if (nutrientLevel < 0 || nutrientLevel > 100)
+                throw std::invalid_argument("nutrientLevel must be between 0 and 100");
+        }
+
+        //Getters
+        double getmoistureLevel() const { return moistureLevel; }
+        double getnutrientLevel() const { return nutrientLevel; }
+        std::string getId() const { return id; }
+
+        // Operator overloading
+        friend std::ostream& operator<<(std::ostream& os, const FieldZone& z) {
+            os << "Zone " << z.id << " [Moisture:" << z.moistureLevel << " Nutrients:" << z.nutrientLevel << "]";
+            return os;
+        }
+
+        bool operator==(const FieldZone& other) const {
+            return id == other.id;
+        }
+
+        // Methods
+        void display() const {
+            std::cout << "Zone: " << id << "\n";
+            std::cout << "Moisture Level: " << moistureLevel << "/100\n";
+            std::cout << "Nutrient Level: " << nutrientLevel << "/100\n";
+        }
 };
 #endif
